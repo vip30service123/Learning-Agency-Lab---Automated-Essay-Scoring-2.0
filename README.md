c42e6b1da73d0b5bf4bd5cb7c04f1b479ab38032
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2024-04-19 22:30:14 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Learning-Agency-Lab---Automated-Essay-Scoring-2.0
+
+
+
+## Vectorizer
+Tf-idf
+
+
+
+## Model 
+XGBoost </br>
+LightGMB </br>
+CatBoost </br>
+RandomForrest </br>
+SVC </br>
+
+
+## Something to do
+#### Preprocessing
+No idea now </br>
+#### Vectorizer
+#### Tune model's params
--- src/model.py
+++ src/model.py
@@ -1,8 +1,17 @@
 from  xgboost import XGBRegressor
+from sklearn.svm import SVC
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
 
 
 def get_model(model_name: str):
     if model_name == "xgboost":
         return XGBRegressor()
+    elif model_name == "svc":
+        return SVC()
+    elif model_name == "lightgbm":
+        return LGBMClassifier()
+    elif model_name == "catboost":
+        return CatBoostClassifier()
     else:
         raise Exception("This model is not available.")
